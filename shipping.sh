55ed35db85d1e1650e4e0b1dd192b7828c910c92
surendevops75 <surendevops75@gmail.com>
2025-10-04 20:19:16 +0530

    roboshop

--- shipping.sh
+++ shipping.sh
@@ -63,14 +63,4 @@ systemctl daemon-reload
 systemctl enable shipping  &>>$LOG_FILE
 
 dnf install mysql -y  &>>$LOG_FILE
-
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
-else
-    echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
-fi
-
-systemctl restart shipping
+VALIDATE $? "installing mysql"
